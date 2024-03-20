1d8e0967841834225207029d362a18710c01b6ed
Davi Azevedo <azvd.davi@gmail.com>
2024-03-19 23:45:44 -0300

    packages and data types

--- src/com/betrybe/helloWorld/HelloWorld.java
+++ src/com/betrybe/helloWorld/HelloWorld.java
@@ -1,3 +1,5 @@
+package com.betrybe.helloWorld;
+
 public class HelloWorld {
     public static void main (String[] args) {
         System.out.print("Primeira linha");
---
+++ src/com/betrybe/helloWorld/RaizQuadradaDeVinteECinco.java
@@ -0,0 +1,12 @@
+package com.betrybe.helloWorld;
+
+import java.lang.*;
+import java.lang.Math;
+
+
+public class RaizQuadradaDeVinteECinco {
+    public static void main(String[] args) {
+        System.out.println("A raiz quadrada de vinte e cinco é:");
+        System.out.println(Math.sqrt(25));
+    }
+}
